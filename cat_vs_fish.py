655fb1c44cab4547c93d5bfdbafb2398e6392774
nguyenannie <tramtramrhinomo@gmail.com>
2018-06-04 15:19:13 +0200

    Create a Settings class for setting up the game

--- cat_vs_fish.py
+++ cat_vs_fish.py
@@ -1,11 +1,13 @@
 import sys
-
 import pygame
+from settings import Settings
 
 def run_game():
 	pygame.init()
-	screen = pygame.display.set_mode((1000, 700))
+	settings = Settings()
+	screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 	pygame.display.set_caption("Cat Vs Fish")
+	screen.fill(settings.background_color)
 
 	while True:
 		for event in pygame.event.get():
