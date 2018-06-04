97fa5520f7fcd5a1f4d75b5e63bc4a26071ef25d
nguyenannie <tramtramrhinomo@gmail.com>
2018-06-04 16:04:42 +0200

    add cat_speed_factor

--- cat_vs_fish.py
+++ cat_vs_fish.py
@@ -10,10 +10,11 @@ def run_game():
 	screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 	pygame.display.set_caption("Cat Vs Fish")
 
-	cat = Cat(screen)
+	cat = Cat(screen, settings)
 
 	while True:
 		gm.check_events(cat)
+		cat.update()
 		gm.update_screen(screen, settings, cat)
 
 run_game()
