53a6a7374f2f3293a5e9663b577ab48f7015c8c4
nguyenannie <tramtramrhinomo@gmail.com>
2018-06-04 15:37:27 +0200

    Add cat class and draw cat on the scree

--- cat_vs_fish.py
+++ cat_vs_fish.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
 from settings import Settings
+from cat import Cat
 
 def run_game():
 	pygame.init()
 	settings = Settings()
 	screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 	pygame.display.set_caption("Cat Vs Fish")
+
+	cat = Cat(screen)
+
 	screen.fill(settings.background_color)
+	cat.blitme()
 
 	while True:
 		for event in pygame.event.get():
